6eec2c850b5d56b2f94c62119baddf9e0a81a135
Gavin de Bruyn <me@gavindebruyn.com>
2022-10-08 09:49:45 +0200

    Initialize age field values
    Signed-off-by: Gavin de Bruyn <me@gavindebruyn.com>

---
+++ src/enums/storage-key.enum.js
@@ -0,0 +1,4 @@
+export const STORAGE_KEY = {
+    MINIMUM_AGE: 'minimumAge',
+    MAXIMUM_AGE: 'maximumAge'
+}
--- src/pages/extension-dialog-page/extension-dialog.page.js
+++ src/pages/extension-dialog-page/extension-dialog.page.js
@@ -1,1 +1,23 @@
-console.log('Extension dialog page js loaded');
+import { STORAGE_KEY } from '../../enums/storage-key.enum';
+
+const PAGE_ELEMENT_ID = {
+    MINIMUM_AGE_INPUT: 'minimum-age-input',
+    MAXIMUM_AGE_INPUT: 'maximum-age-input',
+    SAVE_BUTTON: 'save-button'
+}
+
+initializePage();
+
+function initializePage() {
+    initializeAgeInputs();
+}
+
+function initializeAgeInputs() {
+    chrome.storage.sync.get(STORAGE_KEY.MINIMUM_AGE, result => {
+        document.getElementById(PAGE_ELEMENT_ID.MINIMUM_AGE_INPUT).value = result[STORAGE_KEY.MINIMUM_AGE];
+    });
+
+    chrome.storage.sync.get(STORAGE_KEY.MAXIMUM_AGE, result => {
+        document.getElementById(PAGE_ELEMENT_ID.MAXIMUM_AGE_INPUT).value = result[STORAGE_KEY.MAXIMUM_AGE];
+    })
+}
